2fd93b6e8d444a7e60878723380d61a9c6481874
Scott Motte <mot@mot.la>
2022-09-29 12:24:22 -0700

    Update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 require('dotenv-vault').config({ debug: true })
 
+console.log(process.env) // output for debugging purposes. remove when going to production.
+
 const nextConfig = {
   reactStrictMode: true,
 }
