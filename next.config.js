d9a1dd067a4ac9e1bcc9289dc17f20a7d206b54f
Scott Motte <mot@mot.la>
2022-09-29 10:55:06 -0700

    Log NODE_ENV

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 require('dotenv-vault').config({ debug: true, override: true })
 
+console.log(process.env.NODE_ENV)
+
 const nextConfig = {
   reactStrictMode: true,
 }
